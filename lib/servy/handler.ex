09125398e4f6b13da32e0e35947728ecb82c3c6d
lukewaring <lukewaring@gmail.com>
2020-09-10 21:18:18 -0400

    Refactor route/1 and format_response/1

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -11,11 +11,11 @@ defmodule Servy.Handler do
     end
 
     def parse(request) do
-        [method, path, _] =         
+        [method, path, _] =      
             request
             |> String.split("\n") # request passed as first arg to split/2
             |> List.first
-            String.split(" ")
+            |> String.split(" ")
 
         %{ method: method, path: path, resp_body: "" }
 
@@ -23,18 +23,18 @@ defmodule Servy.Handler do
     end
     
     def route(conv) do
-        # TODO: Create a new map that also has the response body:
-        conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+        %{ conv | resp_body: "Bears, Lions, Tigers" }
+        
+        # conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
     end
     
     def format_response(conv) do
-        # TODO: Use values in the map to create an HTTP response string:
         """
         HTTP/1.1 200 OK
         Content-Type: text/html
-        Content-Length: 20
+        Content-Length: #{byte_size(conv.resp_body)}
     
-        Bears, Lions, Tigers
+        #{conv.resp_body}
         """
     end
 end
