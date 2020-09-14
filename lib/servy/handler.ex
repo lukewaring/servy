37f66a0f29d84dd110f39e0c96e02a7bb1fe2030
lukewaring <lukewaring@gmail.com>
2020-09-14 18:07:05 -0400

    Add route for /about and handle serving about.html

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -70,6 +70,61 @@ defmodule Servy.Handler do
     #     %{ conv | status: 403, resp_body: "Deleting a bear is forbidden!" }
     # end
 
+    def route(%{ method: "GET", path: "/about" } = conv) do
+        # pages_path = Path.expand("../../pages", __DIR__)
+        # file = Path.join(pages_path, "about.html")
+        
+        Path.expand("../../pages", __DIR__)
+        |> Path.join("about.html")
+        |> File.read
+        |> handle_file(conv)
+
+        # See handle_file/2 for how to break down the below case statement into function clauses
+
+        # case File.read(file) do
+        #     {:ok, content} ->
+        #         %{ conv | status: 200, resp_body: content}
+
+        #     {:error, :enoent} ->
+        #         %{ conv | status: 404, resp_body: "File not found!" }
+
+        #     {:error, reason} ->
+        #         %{ conv | status: 500, resp_body: "File error: #{reason}" }
+        # end
+    end
+
+    def handle_file({:ok, content}, conv) do 
+        %{ conv | status: 200, resp_body: content}
+    end
+
+    def handle_file({:error, :enoent}, conv) do 
+        %{ conv | status: 404, resp_body: "File not found!"}
+    end
+
+    def handle_file({:error, reason}, conv) do 
+        %{ conv | status: 500, resp_body: "File error: #{reason}"}
+    end
+
+    # def route(%{ method: "GET", path: "/about" } = conv) do
+    #     # pages_path = Path.expand("../../pages", __DIR__)
+    #     # file = Path.join(pages_path, "about.html")
+
+    #     file =
+    #         Path.expand("../../pages", __DIR__)
+    #         |> Path.join("about.html")
+
+    #     case File.read(file) do
+    #         {:ok, content} ->
+    #             %{ conv | status: 200, resp_body: content}
+
+    #         {:error, :enoent} ->
+    #             %{ conv | status: 404, resp_body: "File not found!" }
+
+    #         {:error, reason} ->
+    #             %{ conv | status: 500, resp_body: "File error: #{reason}" }
+    #     end
+    # end
+    
     def route(%{ path: path } = conv) do
         %{ conv | status: 404, resp_body: "No #{path} here!" }
     end
@@ -155,3 +210,15 @@ IO.puts response
 # response = Servy.Handler.handle(request)
 
 # IO.puts response
+
+request = """
+GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
