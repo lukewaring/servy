cd9f63d62c3247333e8d0900e3e5baad26ef3b0e
lukewaring <lukewaring@gmail.com>
2020-09-15 20:45:13 -0400

    Add module attributes for documentation and pages_path constant

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,4 +1,10 @@
 defmodule Servy.Handler do
+
+    @moduledoc "Handles HTTP requests."
+
+    @pages_path Path.expand("../../pages", __DIR__)
+
+    @doc "Transforms the request into a response."
     def handle(request) do
         request
         |> parse
@@ -13,6 +19,7 @@ defmodule Servy.Handler do
         # format_response(conv)
     end
 
+    @doc "Logs 404 requests."
     def track(%{ status: 404, path: path } = conv) do
         IO.puts "Warning: #{path} is on the loose!"
         conv
@@ -59,9 +66,16 @@ defmodule Servy.Handler do
     end
     
     def route(%{ method: "GET", path: "/bears" } = conv) do 
-        %{ conv | status: 200, resp_body: "Teddy Smokey, Paddington" }
+        %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
     end
     
+    def route(%{ method: "GET", path: "/bears/new" } = conv) do
+        @pages_path
+        |> Path.join("form.html")
+        |> File.read
+        |> handle_file(conv)
+    end
+
     def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
         %{ conv | status: 200, resp_body: "Bear #{id}" }
     end
@@ -71,7 +85,7 @@ defmodule Servy.Handler do
     # end
 
     def route(%{ method: "GET", path: "/about" } = conv) do
-        Path.expand("../../pages", __DIR__)
+        @pages_path
         |> Path.join("about.html")
         |> File.read
         |> handle_file(conv)
@@ -89,13 +103,6 @@ defmodule Servy.Handler do
         #     {:error, reason} ->
         #         %{ conv | status: 500, resp_body: "File error: #{reason}" }
         # end
-    end
-
-    def route(%{ method: "GET", path: "/bears/new" } = conv) do
-        Path.expand("../../pages", __DIR__)
-        |> Path.join("form.html")
-        |> File.read
-        |> handle_file(conv)
     end
     
     def route(%{ path: path } = conv) do
