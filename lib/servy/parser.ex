9115704ee515210ecab266a3ab19975693f0572a
lukewaring <lukewaring@gmail.com>
2020-10-12 19:00:20 -0400

    Add route for POST to /bears; parse params in parse/1 and add params key to Conv struct

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -3,18 +3,32 @@ defmodule Servy.Parser do
   alias Servy.Conv
   
   def parse(request) do
-    [method, path, _] =
-      request
-      # request passed as first arg to split/2
-      |> String.split("\n")
-      |> List.first()
-      |> String.split(" ")
+    [top, params_string] = String.split(request, "\n\n")
+
+    [request_line | header_lines] = String.split(top, "\n")
+
+    [method, path, _] = String.split(request_line, " ")
+
+    params = parse_params(params_string)
+
+    # First approach:
+    # [method, path, _] =
+    #   top
+    #   # request passed as first arg to split/2
+    #   |> String.split("\n")
+    #   |> List.first()
+    #   |> String.split(" ")
 
     %Conv{ 
       method: method,
       path: path,
+      params: params
     }
 
     # [method, path, _] = ["GET", "/wildthings", "HTTP/1.1"]
+  end
+
+  def parse_params(params_string) do 
+    params_string |> String.trim |> URI.decode_query
   end
 end
